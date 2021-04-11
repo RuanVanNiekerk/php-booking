044e45dfc7a4a3e513e6a6c597cf347027d1f361
RuanVanNiekerk <ruanvanniekerk3@gmail.com>
2021-04-11 17:02:53 +0200

    fixed some bugs in the php code

--- booking.php
+++ booking.php
@@ -1,39 +1,73 @@
 <?php
-    $hotels = array(//temporary array to hold hotels. Needs to be changed to seperate database
-        "hotel1" => array("name"=>"test","rate"=>"2","features"=>"test"),
-        "hotel2" => array("name"=>"test2","rate"=>"4","features"=>"test2"),
-        "hotel3" => array("name"=>"test3","rate"=>"6","features"=>"test3"),
-        "hotel4" => array("name"=>"test4","rate"=>"8","features"=>"test4"),
-    );    
-    if(!isset($_SESSION["selection"])){
-        $_SESSION["selection"] = $_POST["hotel"];//hotel that was selected on previous page is used to pull relevant info of that hotel from array/database
-        var_dump($_SESSION["selection"]);
-        $_SESSION["date1"] = date_create($_POST["check_in"]);
-        $_SESSION["date2"] = date_create($_POST["check_out"]);
-        $days = date_diff($_SESSION["date1"],$_SESSION["date2"]);
-        var_dump($days->format("%a"));
-        $_SESSION["email"] = $_POST["email"];
-        $_SESSION["firstName"] = $_POST["first_name"];
-        $_SESSION["lastName"] = $_POST["last_name"];
-    };
-        
-    $firstOption = $hotels[$_SESSION["selection"]];
-    //assign values for compared hotel
+session_start();
+
+$hotels = $_SESSION["hotels"];
+$days = date_diff($_SESSION["date1"],$_SESSION["date2"])->format("%a");
+$firstOption = $hotels[$_SESSION["selection"]];
+//assign values for compared hotel
+if(isset($_POST["compHotel"])){
     $secondOption = $hotels[$_POST["compHotel"]];
+}
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.mailtrap.io';                     // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = '05b0611d206f40';                       // SMTP username
+    $mail->Password   = 'c207557c9562af';                       // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 2525;                                   // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress($_SESSION["email"], $_SESSION["firstName"]);     // Add a recipient
+    $mail->addReplyTo('info@example.com', 'Information');
+
     
-    //mails a confirmation of booking
-    
-    if($_POST["book1"]){
-        echo 'Hello World';
-        var_dump(mail($_SESSION["email"], "Booking", $message));        
+    if(isset($_POST["book1"])){
+        $message = "Dear ".$_SESSION["firstName"]." ".$_SESSION["lastName"]."<br><br>"."The following booking details have been confirmed.<br>"
+                . "Location: ".$firstOption["name"]."<br>"
+                . "Duration: ".$days." days.<br>"
+                . "Price: ".$firstOption["rate"]*$days;
+
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Booking Confirmation';
+        $mail->Body    = $message;
+
+        $mail->send();
+        echo 'Message has been sent';
+    }elseif(isset($_POST["book2"])){
+        $message = "Dear ".$_SESSION["firstName"]." ".$_SESSION["lastName"]."<br><br>"."The following booking details have been confirmed.<br>"
+                . "Location: ".$secondOption["name"]."<br>"
+                . "Duration: ".$days." days.<br>"
+                . "Price: ".$secondOption["rate"]*$days;
+        
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Booking Confirmation';
+        $mail->Body    = $message;
+
+        $mail->send();
+        echo 'Message has been sent';
     }
-        /*if($option == "first"){
-            $message = "Dear ".$_SESSION["firstName"]." ".$_SESSION["lastName"]."\n \n"."Your booking has been confirmed. ";
-            var_dump(mail($_SESSION["email"], "Booking", $message));
-        }elseif($option == "second"){
-            $message = "Dear ".$_SESSION["firstName"]." ".$_SESSION["lastName"]."\n \n"."Your booking has been confirmed. ";
-            mail($_SESSION["email"], "Booking", $message); 
-        }*/
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
 <!DOCTYPE html>
 
@@ -41,36 +75,61 @@
 <head>
     <meta charset="utf-8" />
     <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styleSheet.css">
 </head>
 <body>
-    <h1>Hotel Comparison</h1>
-    <div>
-        <h3><?php echo $firstOption["name"]?></h3>
-        <p>Rate:<?php echo $firstOption["rate"] ?></p>
-        <p>Features:<?php echo $firstOption["features"] ?></p>
-        <p>Cost of Stay: R<?php echo $firstOption["rate"]*$days->format("%a")?></p>.<!--Fix issue: always gets 0. sort out price calculation-->
-    <div>
-        <input name="book1" type="submit" value="book">
-    </div>
-    </div>
-    <div>
-        <form id="comp" name="compare" method="post" action="">
-            <select name="compHotel">
-                <option value="hotel1">Hotel 1</option>
-                <option value="hotel2">Hotel 2</option>
-                <option value="hotel3">Hotel 3</option>
-                <option value="hotel4">Hotel 4</option>
-            </select>
-            <input type="submit" value="compare">
-        </form>
-        <h3><?php echo $secondOption["name"]?></h3>
-        <p>Rate:<?php echo $secondOption["rate"] ?></p>
-        <p>Features:<?php echo $secondOption["features"] ?></p>
-        <p>Cost of Stay: R<?php echo $secondOption["rate"]*$days->format("%a")?></p><!--Fix issue: always gets 0. sort out price calculation-->
-    <div>
-        <input name="book1" type="submit" value="book">
-    </div>
-    </div>
-    <script type="text/javascript" src="submit.js"></script>
+    <div class="container">
+        <div class="row">
+            <h1>Hotel Comparison</h1>
+        </div>  
+        <div class="row">
+            <div class="col-6">
+                <div class="card-custom h-100">
+                    <div class="card-body">
+                        <h3><?php echo $firstOption["name"]?></h3>
+                        <p>Rate:<?php echo $firstOption["rate"] ?></p>
+                        <p>Features:<?php echo $firstOption["features"] ?></p>
+                        <p>Cost of Stay: R<?php echo $firstOption["rate"]*$days?></p><!--Fix issue: always gets 0. sort out price calculation-->
+                    </div>                    
+                    <div class="card-footer">
+                        <form method="post">
+                            <input name="book1" type="submit" value="book">
+                        </form>
+                    </div>
+                </div>                
+            </div>
+            <div class="col-6">
+                <div class="card-custom h-100">
+                    <div class="card-header">
+                        <form id="comp" name="compare" method="post" action="">
+                            <select name="compHotel" onchange="comp.submit()">
+                                <option value="hotel1">Hotel 1</option>
+                                <option value="hotel2">Hotel 2</option>
+                                <option value="hotel3">Hotel 3</option>
+                                <option value="hotel4">Hotel 4</option>
+                            </select>
+                        </form>
+                    </div>
+                    <div class="card-body">                        
+                        <h3><?php echo $secondOption["name"]?></h3>
+                        <p>Rate:<?php echo $secondOption["rate"] ?></p>
+                        <p>Features:<?php echo $secondOption["features"] ?></p>
+                        <p>Cost of Stay: R<?php echo $secondOption["rate"]*$days?></p><!--Fix issue: always gets 0. sort out price calculation-->                        
+                    </div>
+                    <div class="card-footer">
+                        <form method="post">
+                            <input name="book2" type="submit" value="book">
+                        </form>
+                    </div>
+                </div>                
+            </div>
+        </div>
+    </div>    
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,6 +1,25 @@
-<?php session_start();?>
 <?php
-
+session_start();
+    $hotels = array(//temporary array to hold hotels. Needs to be changed to seperate database
+        "hotel1" => array("name"=>"test","rate"=>"2","features"=>"test"),
+        "hotel2" => array("name"=>"test2","rate"=>"4","features"=>"test2"),
+        "hotel3" => array("name"=>"test3","rate"=>"6","features"=>"test3"),
+        "hotel4" => array("name"=>"test4","rate"=>"8","features"=>"test4"),
+    );
+    $_SESSION["hotels"] = $hotels;
+    
+    if(isset($_POST["hotel"])){
+        $_SESSION["selection"] = $_POST["hotel"];
+        var_dump($_SESSION["selection"]);
+        $_SESSION["date1"] = date_create($_POST["check_in"]);
+        $_SESSION["date2"] = date_create($_POST["check_out"]);
+        $_SESSION["email"] = $_POST["email"];
+        $_SESSION["firstName"] = $_POST["first_name"];    
+        var_dump($_SESSION["firstName"]);
+        $_SESSION["lastName"] = $_POST["last_name"];
+        
+        header('Location: booking.php');
+    }
 ?>
 <!DOCTYPE html>
 
@@ -8,9 +27,11 @@
 <head>
     <meta charset="utf-8" />
     <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styleSheet.css">
 </head>
 <body>
-    <form method="post" action="booking.php">
+    <form method="post" action="#">
         <h1>Hotel Reservations</h1><br/>
         <label for="first_name">First Name:</label><br/>
         <input name="first_name" type="text" required><br/>
@@ -20,7 +41,7 @@
         <input name="email" type="text" required><br/>
         <p>Select your Hotel by clicking the dropdown menu below.</p><br/>
         <select name="hotel">
-            <option value="hotel1">Hotel 1</option>
+            <option value="hotel1" selected>Hotel 1</option>
             <option value="hotel2">Hotel 2</option>
             <option value="hotel3">Hotel 3</option>
             <option value="hotel4">Hotel 4</option>
@@ -32,5 +53,10 @@
         <input type="submit">
     </form>
     
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
 </body>
 </html>
