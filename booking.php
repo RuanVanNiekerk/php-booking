e39a8974b9ee0040748b8e901f9a2b5e9b935579
RuanVanNiekerk <ruanvanniekerk3@gmail.com>
2021-04-16 06:08:17 +0200

    UI/UX improvements

--- booking.php
+++ booking.php
@@ -15,20 +15,24 @@ $secondOption = $hotels[$_POST["compHotel"]];
 
     if($choice == 'first'){
         if($secondOption["rate"]<$firstOption["rate"]&&!$secondOption["rate"]==NULL){
-            echo '<p style="color:red">Rate:'. $firstOption["rate"] .'</p>';
+            echo '<p style="background-color:red">Rate:<b>R'. $firstOption["rate"] .' per day</b></p>';
         } elseif($secondOption["rate"]>$firstOption["rate"]) {
-            echo '<p style="color:green">Rate:'.$firstOption["rate"].'</p>';
+            echo '<p style="background-color:green">Rate:<b>R'.$firstOption["rate"].' per day</b></p>';
         }else{
-            echo '<p>Rate:'.$firstOption["rate"].'</p>';
+            echo '<p>Rate:<b>R'.$firstOption["rate"].' per day</b></p>';
         }
     }elseif($choice == 'second'){
-        if($secondOption["rate"]<$firstOption["rate"]&&!$secondOption["rate"]==NULL){
-            echo '<p style="color:green">Rate:'. $secondOption["rate"] .'</p>';
-        } elseif($secondOption["rate"]>$firstOption["rate"]) {
-            echo '<p style="color:red">Rate:'.$secondOption["rate"].'</p>';
+        if($secondOption["rate"]!=NULL){
+            if($secondOption["rate"]<$firstOption["rate"]&&!$secondOption["rate"]==NULL){
+                echo '<p style="background-color:green">Rate:<b>R'. $secondOption["rate"] .' per day</b></p>';
+            } elseif($secondOption["rate"]>$firstOption["rate"]) {
+                echo '<p style="background-color:red">Rate:<b>R'.$secondOption["rate"].' per day</b></p>';
+            }else{
+                echo '<p>Rate:<b>R'.$secondOption["rate"].' per day</b></p>';
+            }
         }else{
-            echo '<p>Rate:'.$secondOption["rate"].'</p>';
-        }
+            echo '<p>Rate: <b>R**** per day</b></p>';
+        }        
     }
 }
 function compareFeatures($choice){
@@ -38,20 +42,25 @@ $secondOption = $hotels[$_POST["compHotel"]];
 
     if($choice == 'first'){
         if(strlen($secondOption["features"])<strlen($firstOption["features"])&&!$secondOption["rate"]==NULL){
-            echo '<p style="color:green">Features:'. $firstOption["features"] .'</p>';
+            echo '<p style="background-color:green">Features:<b>'. $firstOption["features"] .'</b></p>';
         } elseif(strlen($secondOption["features"])>strlen($firstOption["features"])) {
-            echo '<p style="color:red">Features:'.$firstOption["features"].'</p>';
+            echo '<p style="background-color:red">Features:<b>'.$firstOption["features"].'</b></p>';
         }else{
-            echo '<p>Features:'.$firstOption["features"].'</p>';
+            echo '<p>Features:<b>'.$firstOption["features"].'</b></p>';
         }
     }elseif($choice == 'second'){
-        if(strlen($secondOption["features"])<strlen($firstOption["features"])&&!$secondOption["rate"]==NULL){
-            echo '<p style="color:red">Features:'. $firstOption["features"] .'</p>';
-        } elseif(strlen($secondOption["features"])>strlen($firstOption["features"])) {
-            echo '<p style="color:green">Features:'.$firstOption["features"].'</p>';
+        if($secondOption["features"]!=NULL){
+            if(strlen($secondOption["features"])<strlen($firstOption["features"])&&!$secondOption["rate"]==NULL){
+                echo '<p style="background-color:red">Features:<b>'. $firstOption["features"] .'</b></p>';
+            } elseif(strlen($secondOption["features"])>strlen($firstOption["features"])) {
+                echo '<p style="background-color:green">Features:<b>'.$firstOption["features"].'</b></p>';
+            }else{
+                echo '<p>Features:<b>'.$secondOption["features"].'</b></p>';
+            }
         }else{
-            echo '<p>Features:'.$secondOption["features"].'</p>';
+            echo '<p>Features: <b>****</b></p>';
         }
+        
     }
 }
 function comparePrice($choice){
@@ -62,19 +71,23 @@ $days = date_diff($_SESSION["date1"],$_SESSION["date2"])->format("%a");
 
     if($choice == 'first'){
         if($secondOption["rate"]*$days<$firstOption["rate"]*$days&&!$secondOption["rate"]*$days==NULL){
-            echo '<p style="color:red">Cost of Stay: R'. $firstOption["rate"]*$days .'</p>';
+            echo '<p style="background-color:red">Cost of Stay: <b>R'. $firstOption["rate"]*$days .'</b></p>';
         } elseif($secondOption["rate"]*$days>$firstOption["rate"]*$days) {
-            echo '<p style="color:green">Cost of Stay: R'.$firstOption["rate"]*$days.'</p>';
+            echo '<p style="background-color:green">Cost of Stay: <b>R'.$firstOption["rate"]*$days.'</b></p>';
         }else{
-            echo '<p>Cost of Stay: R'.$firstOption["rate"]*$days.'</p>';
+            echo '<p>Cost of Stay: <b>R'.$firstOption["rate"]*$days.'</b></p>';
         }
     }elseif($choice == 'second'){
-        if($secondOption["rate"]*$days<$firstOption["rate"]*$days&&!$secondOption["rate"]*$days==NULL){
-            echo '<p style="color:green">Cost of Stay: R'. $secondOption["rate"]*$days .'</p>';
-        } elseif($secondOption["rate"]*$days>$firstOption["rate"]*$days) {
-            echo '<p style="color:red">Cost of Stay: R'.$secondOption["rate"]*$days.'</p>';
+        if($secondOption["rate"]!=NULL){
+            if($secondOption["rate"]*$days<$firstOption["rate"]*$days&&!$secondOption["rate"]*$days==NULL){
+                echo '<p style="background-color:green">Cost of Stay: <b>R'. $secondOption["rate"]*$days .'</b></p>';
+            } elseif($secondOption["rate"]*$days>$firstOption["rate"]*$days) {
+                echo '<p style="background-color:red">Cost of Stay: <b>R'.$secondOption["rate"]*$days.'</b></p>';
+            }else{
+                echo '<p>Cost of Stay: <b>R'.$secondOption["rate"]*$days.'</b></p>';
+            }
         }else{
-            echo '<p>Cost of Stay: R'.$secondOption["rate"]*$days.'</p>';
+            echo '<p>Cost of Stay: <b>R****</b></p>';
         }
     }
 }
@@ -85,67 +98,56 @@ $days = date_diff($_SESSION["date1"],$_SESSION["date2"])->format("%a");
 <head>
     <meta charset="utf-8" />
     <title></title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Halant&family=Nunito+Sans:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/styleSheet.css">
 </head>
 <body>
-    <div class="container container-custom">
-        <div class="row">
-            <div class="col-12 mt-3 p-3 card-custom">
-                <h1>Lets Compare Some Options First</h1>
-            </div>            
-        </div>  
-        <div class="row">
-            <div class="col-12 mt-3  p-0 pr-lg-2 col-lg-6">
-                <div class="card-custom h-100">
-                    <div class="card-header">
-                        <h3>Original Choice</h3>
-                    </div>
-                    <div class="card-body">
-                        <h3><?php echo $firstOption["name"]?></h3>
-                        <?php compareRate("first")?>
-                        <?php compareFeatures("first")?>
-                        <?php compareprice("first")?>
-                    </div>                    
-                    <div class="card-footer">
-                        <form method="post" action="mail.php">
-                            <input name="book1" type="submit" value="book">
-                        </form>
-                    </div>
-                </div>                
+    <?php echo $hotels[1] ?>
+    <div class="flex-container">
+        <div class="flex-col" method="post" action="#">
+            <h1>Lets Compare Some Options First</h1>
+            <div class="flex-item">
+                <h2>Your First Choice</h2>
             </div>
-            <div class="col-12 mt-3 p-0 pl-lg-2 col-lg-6">
-                <div class="card-custom h-100">
-                    <div class="card-header">
-                        <form id="comp" name="compare" method="post" action="">
-                            <select name="compHotel" onchange="comp.submit()">
-                                <option selected disabled>Choose a Hotel</option>
-                                <option value="hotel1">Hotel 1</option>
-                                <option value="hotel2">Hotel 2</option>
-                                <option value="hotel3">Hotel 3</option>
-                                <option value="hotel4">Hotel 4</option>
-                            </select>
-                        </form>
-                    </div>
-                    <div class="card-body">                        
-                        <h3><?php echo $secondOption["name"]?></h3>
-                        <?php compareRate("second")?>
-                        <?php compareFeatures("second")?>
-                        <?php compareprice("second")?>                        
-                    </div>
-                    <div class="card-footer">
-                        <form method="post" action="mail.php">
-                            <input name="book2" type="submit" value="book">
-                        </form>
-                    </div>
-                </div>                
+            <div class="flex-item">
+                <h3><?php echo $firstOption["name"]?></h3>
+                <?php compareRate("first")?>
+                <?php compareFeatures("first")?>
+                <?php compareprice("first")?>
+                <form method="post" action="mail.php">
+                    <input id="button" name="book1" type="submit" value="Make a Booking">
+                </form>
+            </div>
+            <div class="flex-item">
+                <h2>Select Your Second Choice</h2>
+                <form id="comp" name="compare" method="post" action="">
+                    <select name="compHotel" onchange="comp.submit()">
+                        <option selected disabled>Choose a Hotel</option>
+                        <option value="hotel1">Lemon Tree Lane</option>
+                        <option value="hotel2">The Paxton Hotel</option>
+                        <option value="hotel3">Radison Blu Hotel</option>
+                        <option value="hotel4">Forest Hall B&B</option>
+                    </select>
+                </form> 
+            </div>
+            <div class="flex-item">
+                                       
+                <h3><?php 
+                    if($secondOption["name"]!=NULL){
+                        echo $secondOption["name"];
+                    }else{
+                        echo "****";
+                    }
+                ?></h3>
+                <?php compareRate("second")?>
+                <?php compareFeatures("second")?>
+                <?php compareprice("second")?>
+                <form method="post" action="mail.php">
+                    <input id="button" name="book2" type="submit" value="Make a Booking">
+                </form>
             </div>
         </div>
-    </div>    
-    
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
     $hotels = array(//temporary array to hold hotels. Needs to be changed to seperate database
-        "hotel1" => array("name"=>"test","rate"=>"2","features"=>"test"),
-        "hotel2" => array("name"=>"test2","rate"=>"4","features"=>"test2test2"),
-        "hotel3" => array("name"=>"test3","rate"=>"6","features"=>"test3test3test3"),
-        "hotel4" => array("name"=>"test4","rate"=>"8","features"=>"test4test4test4"),
+        "hotel1" => array("name"=>"Lemon Tree Lane","rate"=>"180","features"=>"Pool, Beatifully Furnished Rooms, Communal Breakfast each Morning"),
+        "hotel2" => array("name"=>"The Paxton Hotel","rate"=>"280","features"=>"Close to the Beach, Pool, Bar, Free Wifi, Room Service, Airport Shuttle Service"),
+        "hotel3" => array("name"=>"Radison Blu Hotel","rate"=>"320","features"=>"Close to the Beach, Pool, Bar, Free Wifi, Room Service, Airport Shuttle Service, Wellness Center and Spa, Sea View"),
+        "hotel4" => array("name"=>"Forest Hall B&B","rate"=>"120","features"=>"Pool, Free Parking, Airport Shuttle Service, Satellite TV, Breakfast Buffet and Home Cooked Meals"),
     );
     $_SESSION["hotels"] = $hotels;
     
@@ -27,43 +27,45 @@ session_start();
 <head>
     <meta charset="utf-8" />
     <title></title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Halant&family=Nunito+Sans:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/styleSheet.css">
 </head>
 <body>
-    <div class="container">
-        <div class="row m-3">
-            <div class="col-12 p-3 card-custom">
-                <form method="post" action="#">
-                <h1>Hotel Reservations</h1><br/>
-                <label for="first_name">First Name:</label><br/>
-                <input name="first_name" type="text" required><br/>
-                <label for="last_name">Surname:</label><br/>
-                <input name="last_name" type="text" required><br/>
-                <label for="email">Email address:</label><br/>
-                <input name="email" type="text" required><br/>
-                <p>Select your Hotel by clicking the drop-down menu below.</p><br/>
-                <select name="hotel">
-                    <option value="hotel1" selected>Hotel 1</option>
-                    <option value="hotel2">Hotel 2</option>
-                    <option value="hotel3">Hotel 3</option>
-                    <option value="hotel4">Hotel 4</option>
-                </select><br/>
-                <label for="check_in">Check in Date</label>
-                <input name="check_in" type="date" required><br/>
-                <label for="check_out">Check out Date</label>
-                <input name="check_out" type="date" required><br/>
-                <input type="submit">
-                </form>
-            </div>
+    <div>
+        <div class="flex-container">
+            <form class="flex-col" method="post" action="#">
+                <div class="flex-item">
+                    <h1>Hotel Reservations</h1>
+                    <label for="first_name">First Name:</label><br/>
+                    <input class="input" name="first_name" type="text" required><br/><br/>
+                    <label for="last_name">Surname:</label><br/>
+                    <input class="input" name="last_name" type="text" required><br/><br/>
+                    <label for="email">Email address:</label><br/>
+                    <input class="input" name="email" type="text" required><br/>
+                </div>
+                <div class="flex-item">
+                    <p>Select your Hotel.</p>
+                    <input type="radio" name="hotel" value="hotel1" required>
+                    <label for="hotel1">Lemon Tree Lane</label>
+                    <input type="radio" name="hotel" value="hotel2">
+                    <label for="hotel2">The Paxton Hotel</label>
+                    <input type="radio" name="hotel" value="hotel3">
+                    <label for="hotel3">Radison Blu Hotel</label>
+                    <input type="radio" name="hotel" value="hotel4">
+                    <label for="hotel4">Forest Hall B&B</label>
+                </div>
+                <div class="flex-item">
+                    <label for="check_in">Check in Date</label>
+                    <input name="check_in" type="date" required>
+                    <label for="check_out">Check out Date</label>
+                    <input name="check_out" type="date" required><br/>
+                    <input id="button" type="submit">
+                </div>
+            </form>
         </div>  
     </div>
-        
-    
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  
 </body>
 </body>
 </html>
